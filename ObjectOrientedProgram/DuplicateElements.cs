2691e0105a46fa698a598b05387c6eb45a6db01f
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-15 17:06:12 +0530

    [somu]Add:Counting Frequency of Each Element of an Array.

--- ObjectOrientedProgram/DuplicateElements.cs
+++ ObjectOrientedProgram/DuplicateElements.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ObjectOrientedProgram
 {
@@ -10,38 +11,78 @@ namespace ObjectOrientedProgram
     {
         public void DuplicateNumbers()
         {
-            int[] num = { 2,2,3,4,5,5,6,7,7,8 };
+            int[] num = { 2, 2, 3, 4, 5, 5, 6, 7, 7, 8 };
             int duplicate = 0;
-            for(int i=0; i<num.Length;i++)
+            for (int i = 0; i < num.Length; i++)
             {
-                for(int j=i+1; j < num.Length;j++)
+                for (int j = i + 1; j < num.Length; j++)
                 {
                     if (num[i] == num[j])
                     {
                         duplicate++;
                     }
-                }                
+                }
             }
-           Console.WriteLine(duplicate);
+            Console.WriteLine(duplicate);
         }
         public void PrintUniqueElements()
         {
-            int[] array = {2,3,4,5,1,2,3,4,5,6,7,7,8};
-            for(int i=0;i<array.Length;i++)
+            int[] array = { 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 7, 7, 8 };
+            for (int i = 0; i < array.Length; i++)
             {
                 bool duplicate = true;
-                for(int j=0;j<array.Length;j++)
+                for (int j = 0; j < array.Length; j++)
                 {
-                    if (array[i] == array[j] && i!=j)
+                    if (array[i] == array[j] && i != j)
                     {
                         duplicate = false;
                         break;
                     }
                 }
-                if(duplicate)
+                if (duplicate)
                 {
                     Console.WriteLine(array[i]);
                 }
+            }
+        }
+        public void FrequencyOfEachElementInArray()
+        {
+            int[] array = { 1, 2, 2, 3, 4, 4, 5, 6, 6, 7, 7, 8 };
+
+            // Create arrays to store unique elements and their frequencies
+            int[] uniqueElements = new int[array.Length];
+            int[] frequencies = new int[array.Length];
+
+            int uniqueCount = 0;
+
+            // Loop through each element in the array
+            for (int i = 0; i < array.Length; i++)
+            {
+                int element = array[i];
+                bool found = false;
+
+                for (int j = 0; j < uniqueCount; j++)
+                {
+                    if (uniqueElements[j] == element)
+                    {
+                        // Increment the frequency if found
+                        frequencies[j]++;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    uniqueElements[uniqueCount] = element;
+                    frequencies[uniqueCount] = 1;
+                    uniqueCount++;
+                }
+            }
+            // Print the frequencies
+            Console.WriteLine("Element   Frequency");
+            for (int i = 0; i < uniqueCount; i++)
+            {
+                Console.WriteLine($"{uniqueElements[i]}         {frequencies[i]}");
             }
         }
     }
--- ObjectOrientedProgram/Program.cs
+++ ObjectOrientedProgram/Program.cs
@@ -4,9 +4,29 @@
     {
         public static void Main(string[] args)
         {
-            DuplicateElements duplicate = new DuplicateElements();
-            //duplicate.DuplicateNumbers();
-            duplicate.PrintUniqueElements();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose Option to Perform\n1.Duplicate Elements\n2.Unique Elements\n3.Frequency of Each Element\n4.Exit");
+                int option=Convert.ToInt32(Console.ReadLine());
+                DuplicateElements duplicate = new DuplicateElements();
+
+                switch (option)
+                {
+                    case 1:
+                        duplicate.DuplicateNumbers();
+                        break;
+                    case 2:
+                        duplicate.PrintUniqueElements();
+                        break;
+                        case 3:
+                        duplicate.FrequencyOfEachElementInArray();
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
