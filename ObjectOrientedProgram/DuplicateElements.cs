0c19212d2f4b68a01d5556e92f596e7299069664
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-15 17:25:19 +0530

    [somu]Add:Finding Maximum and Minimum Element in an Array.

--- ObjectOrientedProgram/DuplicateElements.cs
+++ ObjectOrientedProgram/DuplicateElements.cs
@@ -85,5 +85,23 @@ namespace ObjectOrientedProgram
                 Console.WriteLine($"{uniqueElements[i]}         {frequencies[i]}");
             }
         }
+        public void FindMaximumAndMinimum()
+        {
+            int[] array = { 1, 9, 43, 6, 3, 7, 8,23, 10 };
+            int max = array[0];
+            int min = array[0];
+            for(int i=0; i < array.Length; i++)
+            {
+                if (array[i]>max)
+                {
+                    max = array[i];
+                }
+                if (array[i]<min)
+                {
+                    min = array[i];
+                }
+            }
+            Console.WriteLine("Maximum :{0}  Minimum : {1}",max,min);
+        }
     }
 }
--- ObjectOrientedProgram/Program.cs
+++ ObjectOrientedProgram/Program.cs
@@ -7,7 +7,7 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Choose Option to Perform\n1.Duplicate Elements\n2.Unique Elements\n3.Frequency of Each Element\n4.Exit");
+                Console.WriteLine("Choose Option to Perform\n1.Duplicate Elements\n2.Unique Elements\n3.Frequency of Each Element\n4.Find Max And Min element in an Array\n5.Exit");
                 int option=Convert.ToInt32(Console.ReadLine());
                 DuplicateElements duplicate = new DuplicateElements();
 
@@ -22,7 +22,10 @@
                         case 3:
                         duplicate.FrequencyOfEachElementInArray();
                         break;
-                    case 4:
+                        case 4:
+                        duplicate.FindMaximumAndMinimum();
+                        break;
+                    case 5:
                         flag = false;
                         break;
                 }
